75dba6a9b901d837cf95e2b13278b79e62fd171f
lgjoseluis <lgjoseluis@hotmail.com>
2022-09-11 20:00:24 -0500

    Create song API project

---
+++ src/Karaoke.Song.Api/Controllers/SongController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Karaoke.Song.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private readonly ILogger<SongController> _logger;
+
+        public SongController(ILogger<SongController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Daddy Cool", "Rasputin", "Rivers of Babylon", "Ma Baker"};
+        }
+    }
+}
